f955092fe74e0dfea64d5af78647bbedfd9ea174
lana002 <lanasalem020@gmail.com>
2025-05-27 15:14:21 +0300

    url change

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,11 @@ const nextConfig = {
         return [
             {
                 source: '/api/pokemon',
-                destination: 'https://sxd6x1vm59.execute-api.eu-west-2.amazonaws.com/Prod/pokemon',
+                destination: 'https://cdewjecuj3.execute-api.eu-west-2.amazonaws.com/Prod/pokemon',
             },
             {
                 source: '/api/pokemon/:id',
-                destination: 'https://sxd6x1vm59.execute-api.eu-west-2.amazonaws.com/Prod/pokemon/:id',
+                destination: 'https://cdewjecuj3.execute-api.eu-west-2.amazonaws.com/Prod/pokemon/:id',
             },
         ];
     }
