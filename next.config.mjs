6973882604caa06fd57719ee860e6b5151574db7
lana002 <lanasalem020@gmail.com>
2025-05-27 14:22:11 +0300

    updated to correct backend url

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,11 @@ const nextConfig = {
         return [
             {
                 source: '/api/pokemon',
-                destination: 'https://p6txghrt9c.execute-api.eu-west-2.amazonaws.com/Prod/pokemon',
+                destination: 'https://cdewjecuj3.execute-api.eu-west-2.amazonaws.com/Prod/Prod/pokemon',
             },
             {
                 source: '/api/pokemon/:id',
-                destination: 'https://p6txghrt9c.execute-api.eu-west-2.amazonaws.com/Prod/pokemon/:id',
+                destination: 'https://cdewjecuj3.execute-api.eu-west-2.amazonaws.com/Prod/Prod/pokemon/:id',
             },
         ];
     }
